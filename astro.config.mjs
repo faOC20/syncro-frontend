e479e0776da0bc623407ba548749a99a5bba0773
faOC20 <fabianortizfc@gmail.com>
2025-04-23 16:13:19 -0400

    feat(config): add environment schema for BACK_API in astro config
    This commit introduces an environment schema for the BACK_API variable in the Astro configuration, allowing it to be accessed publicly on the client side. This change ensures that the environment variable is properly validated and accessible where needed.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 
+
 // https://astro.build/config
 export default defineConfig({
+  env: {
+    schema:{
+      BACK_API: envField.string({context: 'client', access: 'public'}),
+    }
+  },
   output: 'server',
   adapter: node({mode:'standalone'}),
   server: {
