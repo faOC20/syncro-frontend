a1d012a10913fc5c6fac77c39eb741c99c20a5a8
faOC20 <fabianortizfc@gmail.com>
2025-05-01 10:05:40 -0400

    feat: add React integration and update dependencies
    Added @astrojs/react integration to enable React support in the Astro project. Updated tsconfig.json to include React JSX settings and added necessary React-related dependencies to package.json.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 
 
+import react from '@astrojs/react';
+
+
 // https://astro.build/config
 export default defineConfig({
   env: {
@@ -11,13 +14,18 @@ export default defineConfig({
       BACK_API: envField.string({context: 'client', access: 'public'}),
     }
   },
+
   output: 'server',
   adapter: node({mode:'standalone'}),
+
   server: {
     host: "0.0.0.0",
     port: 8080,
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react()],
 });
